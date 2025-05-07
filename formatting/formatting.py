6c41cc1ccbea29c46f530a371d538757a6e8fa94
Malgorzata Bulhak <malgorzata.bulhak@studenci.collegiumwitelona.pl>
2025-05-07 19:47:42 +0200

    Poprawki

--- formatting/formatting.py
+++ formatting/formatting.py
@@ -1,21 +1,22 @@
-from tkinter import messagebox, TclError
+import tkinter as tk
+from tkinter import messagebox
 
-def toggle_bold():
+def toggle_bold(text_widget):
     try:
-        current_tags = text_area.tag_names("sel.first")
+        current_tags = text_widget.tag_names("sel.first")
         if "bold" in current_tags:
-            text_area.tag_remove("bold", "sel.first", "sel.last")
+            text_widget.tag_remove("bold", "sel.first", "sel.last")
         else:
-            text_area.tag_add("bold", "sel.first", "sel.last")
+            text_widget.tag_add("bold", "sel.first", "sel.last")
     except tk.TclError:
         messagebox.showwarning("Uwaga", "Zaznacz tekst, który chcesz pogrubić")
 
-def toggle_italic():
+def toggle_italic(text_widget):
     try:
-        current_tags = text_area.tag_names("sel.first")
+        current_tags = text_widget.tag_names("sel.first")
         if "italic" in current_tags:
-            text_area.tag_remove("italic", "sel.first", "sel.last")
+            text_widget.tag_remove("italic", "sel.first", "sel.last")
         else:
-            text_area.tag_add("italic", "sel.first", "sel.last")
+            text_widget.tag_add("italic", "sel.first", "sel.last")
     except tk.TclError:
         messagebox.showwarning("Uwaga", "Zaznacz tekst, który chcesz pochylić")
